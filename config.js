382e142bca6495ea98a00b422054b0a071365db2
Bitex Kuang <i@bitex.me>
2015-08-12 11:41:51 +0800

    Updated config.js.

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 var _config = {
-  
+    
     // Title of your own AnimeHub.
     title: 'AnimeHub',
-  
+    
     // Your username.
     username: '',
-  
+    
     // Your AnimeHub repo name.
     repo: 'AnimeHub',
     
     // Reviews to show per page.
     per_page: 20,
-  
+    
     // Your access token for AnimeHub.
     access_token: '',
-  
+    
     // ID of your AniList client.
     anilist_client_id: '',
-  
+    
     // Secret of your AniList client.
     anilist_client_secret: '',
-  
+    
     // Custom background image.
     background_url: ''
-
+    
 }
