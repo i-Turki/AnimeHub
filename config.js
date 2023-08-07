2d6d5673117f5b0a571407fd624d493238e596df
Turki Al-ghamdi <77016791+i-Turki@users.noreply.github.com>
2023-08-07 17:35:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 var _config = {
 
     // Title of your AnimeHub.
-    title: 'AnimeHub',
+    title: 'SkyTV',
 
     // Your GitHub username.
-    username: '',
+    username: 'i-Turki',
 
     // Your GitHub repo for anime reviews.
-    repo: '',
+    repo: 'gh-pages',
 
     // Which of the issues should be shown? (all / user / label)
     issue_filter: 'all',
 
     // Issues created by this user will be shown if issue_filter is set to 'user'.
-    issue_filter_username: '',
+    issue_filter_username: 'user',
 
     // Issues with this label will be shown if issue_filter is set to 'label'.
-    issue_filter_label: '',
+    issue_filter_label: 'label',
 
     // Reviews to show per page.
     per_page: 20,
 
     // Your access token for AnimeHub.
-    access_token: '',
+    access_token: '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',
 
     // ID of your AniList client.
-    anilist_client_id: '',
+    anilist_client_id: '13931',
 
     // Secret of your AniList client.
-    anilist_client_secret: '',
+    anilist_client_secret: '1jM7JfbDn55NkFQNXy8TwUTNYzmdxmm7cBbwtCbY',
 
     // Custom background image.
-    background_url: ''
+    background_url: 'https://media.discordapp.net/attachments/944149680529494066/1135296714522239006/67e80bc7bc548ade.png'
 
 }
